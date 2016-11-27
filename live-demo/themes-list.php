7e198f419e0823f2598351e63c9fca28cf64609c
Chuckie Chang <546007561@qq.com>
2016-11-27 16:08:22 +0800

    Update

--- live-demo/themes-list.php
+++ live-demo/themes-list.php
@@ -1,6 +1,6 @@
 <?php
 /** Sets up the WordPress Environment. */
-header( 'Content-Type:text/html;charset=utf-8' );
+header( 'Content-Type:application/x-javascript;charset=utf-8' );
 require '../../../../wp-load.php';
 
 
