c6f8af6aeda924b08c6feb7a5833e4372eb02abe
UIUX Lab <uiuxlab@gmail.com>
2019-01-23 23:15:00 +0800

    Update

--- admin/uix-custom-metaboxes/init.php
+++ admin/uix-custom-metaboxes/init.php
@@ -108,6 +108,9 @@ if ( !class_exists( 'Uix_Products_Custom_Metaboxes' ) ) {
 					//Colorpicker
 					wp_enqueue_style( 'wp-color-picker' );
 					wp_enqueue_script( 'wp-color-picker' );	
+				  
+				    //date picker
+				    wp_enqueue_script('jquery-ui-datepicker');
 	
 			  }
 			
